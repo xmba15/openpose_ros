ae598a61b76f4d7e2ce96b997174304680bfa865
bajsk <tran@jsk.imi.i.u-tokyo.ac.jp>
2017-07-21 21:44:37 +0900

    modify script

--- scripts/skeleton_number.py
+++ scripts/skeleton_number.py
@@ -2,12 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import rospy
-import numpy as np
-import cv_bridge
-import message_filters
-import 
 from openpose_ros_msgs.msg import PeoplePoseArray as PDA
-from sensor_msgs.msg import Image
 
 def callback(data):
     rospy.loginfo("%d", len(data.poses))
